28988613eb8c8497c17ccb85ed0c6102c70d5e77
Pratyusha Priyadarsani <pratyushapriyadarshini31@gmail.com>
2026-07-02 11:39:54 +0530

    Added Week 1 Decorator Pattern Exercise

---
+++ Week-1/DecoratorPatternExample/DecoratorPatternExample/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Notifier notifier =
+                new SlackNotifierDecorator(
+                        new SMSNotifierDecorator(
+                                new EmailNotifier()));
+
+        notifier.send("Decorator Pattern Example");
+    }
+}
+
